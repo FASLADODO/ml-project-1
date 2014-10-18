ba72bb1613d2b415f96542b9f70af322d993f138
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-10-18 21:15:20 +0200

    Least squares and ridge regression (from past weeks)

--- src/leastSquares.m
+++ src/leastSquares.m
@@ -1,4 +1,4 @@
 function beta = leastSquares(y, tX)
 % Fit a linear model using Least Squares (exact method)
-
+  beta = (tX' * tX) \ (tX' * y);
 end
--- src/ridgeRegression.m
+++ src/ridgeRegression.m
@@ -2,4 +2,8 @@ function beta = ridgeRegression(y, tX, alpha)
 % Learn model parameters beta using Ridge Regression
 % alpha: gradient descent step size
 
+  gramMatrix = (tX' * tX);
+  l = lambda * eye(size(gramMatrix));
+  beta = (gramMatrix + l) \ (tX' * y);
+
 end
