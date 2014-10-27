f3af715b59a9e2aa633ae5b54a6082f3d64d311e
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-10-27 13:35:23 +0100

    Data analysis: confirm separation along X35

--- src/dataAnalysis.m
+++ src/dataAnalysis.m
@@ -18,11 +18,11 @@ X(:,1:35) = normalized(X(:,1:35));
 
 hist(y);
 
-% there is one main gaussian + one other thing that we cannot understand :
-% too much data to be outliers : a second gaussian maybe ?
+% there is one main gaussian + one other thing that we cannot understand:
+% too much data to be outliers: a second gaussian maybe ?
 
 
-%% Input Visualization
+%% Input features visualization
 figure;
 side = 7;
 for k = 1:size(X, 2)
@@ -45,8 +45,44 @@ for k = 1:size(X, 2)
     plot(X(:, k), y_train, '.');
     title(['X', int2str(k), ' versus Y']);
 end;
-% Note that features 37 to 44 have discrete values!
+% There is a certain number of datapoints with high Y (output) value.
+% This seems uncorrelated to all features except X35, which may enable us
+% to separate these points.
+% X19 and X26 look linearly correlated to Y.
+% Note that features 37 to 44 have discrete values.
 
+%% First try at separating the two models
+% We suppose our data indeed follows two different models:
+% 1. If X35 > some value, y = large constant => one parameter model
+% 2. Else, y follows another, more complex model
+
+t = y > 6000;
+figure;
+side = 7;
+for k = 1:size(X, 2)
+    subplot(side, side, k);
+    plot(X(t, k), y(t), '.r');
+    hold on;
+    plot(X(~t, k), y(~t), '.b');
+    title(['X', int2str(k)]);
+end;
+
+% Interesting plot to put in the report to show the categorical variables
+% are not correlated with the hypothetical two models 
+
+t = X(:, 35) > 1;
+figure;
+for k = 1:size(X, 2)
+    subplot(side, side, k);
+    plot(X(t, k), y(t), '.r');
+    hold on;
+    plot(X(~t, k), y(~t), '.b');
+    title(['X', int2str(k)]);
+end;
+
+% Indeed, separating with respect to a threshold on X35 gives good results.
+% Could we learn that threshold automatically to minimize error? (just a
+% grid search on the threshold value?)
 
 %% Plotting the features against each other
 figure;
@@ -62,22 +98,6 @@ end;
 
 % We spot some correlations (but not that many).
 % Use ACP for dimensionality reduction?
-
-%% First try at separating the two models
-
-t = y>6000;
-figure;
-side = 7;
-for k = 1:size(X, 2)
-    subplot(side, side, k);
-    plot(X(t, k), y(t), '.r');
-    hold on;
-    plot(X(~t, k), y(~t), '.b');
-    title(['X', int2str(k)]);
-end;
-
-% Interesting plot to put in the report to show the categorical variables
-% are not correlated with the hypothetical two models 
 
 %% Compute the correlation between the features and spot the largest ones
 
