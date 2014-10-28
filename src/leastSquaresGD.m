a9851725bf39dfdcd7e848a82476c9c987ca889e
Krishna Sapkota <krishnasapkota@tsf-476-wpa-3-201.epfl.ch>
2014-10-28 13:41:55 +0100

    added leastSquareGD

--- src/leastSquaresGD.m
+++ src/leastSquaresGD.m
@@ -2,4 +2,35 @@ function beta = leastSquaresGD(y, tX, alpha)
 % Fit a linear model using Least Squares (using gradient descent)
 % alpha: gradient descent step size
 
+  % algorithm parametesfor maximum iterations and convergence
+  maxIters = 1000;
+  convergence_th = 1e-05; % convergence threshold
+
+  % initialize
+  beta = zeros(size(tX, 2), 1);
+
+  L_last = 0;
+  
+  % iterate
+  for k = 1:maxIters
+    % compute gradient 
+    g = computeGradient(y, tX, beta);
+
+    % compute cost
+    L = computeMse(y, tX, beta);
+
+    % update beta
+    beta = beta - alpha * g;
+
+    % check convergence
+    if abs(L_last - L) < convergence_th
+    	break;
+    end 
+
+    L_last = L;
+
+  end
+
+
+
 end
