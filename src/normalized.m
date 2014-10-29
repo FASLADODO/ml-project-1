a98bb96eae2e33ba9a559af2feb53b9746e9f93a
Jade Copet <jade.copet@gmail.com>
2014-10-29 20:33:51 +0100

    Correction on normalized for fat matrix

--- src/normalized.m
+++ src/normalized.m
@@ -1,5 +1,5 @@
 function y = normalized(x)
-  means = ones(length(x), 1) * mean(x);
-  deviations = ones(length(x), 1) * std(x);
+  means = ones(size(x,1), 1) * mean(x);
+  deviations = ones(size(x,1), 1) * std(x);
   y = (x - means) ./ deviations;
 end
