b2c85c4ebee34520199582e2218bdb288f51a32c
Krishna Sapkota <krishnasapkota@Krishnas-MacBook-Pro.local>
2014-11-01 19:40:02 +0100

    added k-CV to learn lambda automatically

--- src/penLogisticRegressionAuto.m
+++ src/penLogisticRegressionAuto.m
@@ -1,50 +1,85 @@
-function [betaStar, trainingErr, testErr] = penLogisticRegressionAuto(y, tX, proportion, k, lambdaValues)
-% Learn model parameters beta and best lambda using k-fold cross-validation
+function [betaStar, trainingErr, testErr] = penLogisticRegressionAuto(y, tX, K, lambdaValues)
+% Learn model parameters beta and best lambda using K-fold cross-validation
 % Plot the error respective to lambda
 %
-% split: proportion of train vs data to use
-% k: number of CV folds
+% K: number of CV folds
 % lambdaValues: values to try out as lambda parameters
+
     if(nargin < 3)
-        proportion = 0.8;
+       K = 5; 
     end;
     if(nargin < 4)
-       k = 5; 
+       lambdaValues = logspace(0, 1, 100); 
     end;
-    if(nargin < 5)
-       lambdaValues = logspace(-2, 2, 100); 
-    end;
+
     n = length(lambdaValues);
-    alpha = 1; % Step size
-    
-    % Train / test split
-    [tXTr, yTr, tXTe, yTe] = split(y, tX, proportion);
+    alpha = 1e-3; % Step size
     
     trainingErr = zeros(n, 1);
     testErr = zeros(n, 1);
     bestErr = -1;
-    for i = 1:length(lambdaValues)
+
+    % split data in k fold (create indices only)
+    setSeed(1);
+    N = size(y,1);
+    idx = randperm(N);
+    Nk = floor(N/K);
+    for k = 1:K
+        idxCV(k,:) = idx(1+(k-1)*Nk:k*Nk);
+    end
+    
+    % tryout all lambda values
+    for i = 1:n
         lambda = lambdaValues(i);
         
-        % TODO: k-fold cross-validation
-        beta = penLogisticRegression(yTr, tXTr, alpha, lambda);
-        
-        trainingErr(i, :) = computeLogisticRegressionMse(yTr, tXTr, beta);
-        testErr(i, :) = computeLogisticRegressionMse(yTe, tXTe, beta);
+        % k-fold cross-validation
+        for k = 1:K
+            % get k'th subgroup in test, others in train
+            idxTe = idxCV(k,:);
+            idxTr = idxCV([1:k-1 k+1:end],:);
+            idxTr = idxTr(:);
+            yTe = y(idxTe);
+            XTe = tX(idxTe,:);
+            yTr = y(idxTr);
+            XTr = tX(idxTr,:);
 
-        if(testErr(i, :) < bestErr || bestErr < 0)
+            % train beta on training data 
+            beta = penLogisticRegression(yTr, XTr, alpha, lambda);
+
+            % compute training and test MSE
+            mseTrSub(k) = computeLogisticRegressionMse(yTr, XTr, beta); 
+            mseTeSub(k) = computeLogisticRegressionMse(yTe, XTe, beta); 
+
+        end
+
+        trainingErr(i, :) = mean(mseTrSub);
+        testErr(i, :) = mean(mseTeSub);
+
+        if(testErr(i, :) < bestErr || bestErr < 0) % best beta is the one for which the test error is the least
             betaStar = beta;
             bestErr = testErr(i, :);
-            %fprintf('Error %f obtained with lambda = %f\n', bestErr, lambda);
         end;
     end;
     
     % Plot evolution of train and test error with respect to lambda
-    
     figure;
     semilogx(lambdaValues, trainingErr, '.-b');
     hold on;
     semilogx(lambdaValues, testErr, '.-r');
     xlabel('Lambda');
     ylabel('Training (blue) and test (red) error');
+
 end
+
+
+
+function setSeed(seed)
+% set seed
+    global RNDN_STATE  RND_STATE
+    RNDN_STATE = randn('state');
+    randn('state',seed);
+    RND_STATE = rand('state');
+    %rand('state',seed);
+    rand('twister',seed);
+end
+
