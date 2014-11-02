685981c7594b9ca8b72037234328b2f9b9ae03f8
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-11-02 13:49:19 +0100

    More efficient polynomial basis expansion

--- src/createPoly.m
+++ src/createPoly.m
@@ -1,10 +1,12 @@
-function Xpoly = createPoly(X,degree)
+function Xpoly = createPoly(X, degree)
 % build matrix Phi for polynomial regression of a given degree
-    Xpoly = [];
-    for i = 1:size(X,2)
-        for k = 1:degree
-            pol(:,k) = X(:,i).^k;
+    N = size(X, 1); % Number of data examples
+    D = size(X, 2); % Initial dimensionality of X
+    
+    Xpoly = [X zeros(N, (degree-1) * D)];
+    for k = 2:degree
+        for i = 1:D
+            Xpoly(:, (k-1) * D + i) = X(:,i) .^ k;
         end
-        Xpoly = [Xpoly pol];
     end
 end
