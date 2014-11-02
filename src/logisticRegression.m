05463dbb3109849fe34169f342ac30024cf0e6e6
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-11-02 12:15:45 +0100

    Learn the model separation automatically with a binary classifier

--- src/logisticRegression.m
+++ src/logisticRegression.m
@@ -1,6 +1,10 @@
 function beta = logisticRegression(y, tX, alpha)
 % Fit a linear model using Logistic Regression (using Newton's method)
 % alpha: step size
+    if(nargin < 3)
+        alpha = 0.1;
+    end;
+
     beta = penLogisticRegression(y, tX, alpha, 0);
 end
 
--- src/separateDataSet.m
+++ src/separateDataSet.m
@@ -1,12 +1,28 @@
-function [X_m1, y_m1, X_m2, y_m2] = separateDataSet(X, y, featureNo)
-% TODO : learn the threshold automatically (or even learn a full
-% classifier)
-    threshold = 1.2;
-    ms = X(:,featureNo) > threshold;
+function [beta, tX1, y1, tX2, y2] = separateDataSet(y, tX, threshold)
+% Assuming our output y can be explained by two distinct model,
+% we try and learn a classifier on the input tX to distinguish between the
+% two. We use the given threshold on the output value to create the two
+% classes.
+    c1 = (y > threshold);
+    binary = false(size(y));
+    binary(c1) = true();
     
-    X_m1 = X(ms,:);
-    y_m1 = y(ms);
+    beta = logisticRegression(binary, tX);
+    yPredicted = binaryPrediction(tX, beta);
+    c1 = (yPredicted == 0);
     
-    X_m2 = X(~ms,:);
-    y_m2 = y(~ms);
+    % Output the split dataset
+    tX1 = tX(c1,:);
+    y1 = y(c1);
+    tX2 = tX(~c1,:);
+    y2 = y(~c1);
+    
+    % Check the validity of the classifier visually
+    figure;
+    for i = 1:25
+        subplot(5, 5, i);
+        hold on;
+        plot(tX1, y1, '.b');
+        plot(tX2, y2, '.r');
+    end;
 end
