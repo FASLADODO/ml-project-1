5f01c28e297f25c7fa5c780148d07e059704557b
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-11-03 03:29:18 +0100

    Fix log reg using ridge reg for some reason

--- src/penLogisticRegressionAuto.m
+++ src/penLogisticRegressionAuto.m
@@ -45,7 +45,7 @@ function [beta, trainingErr, testErr] = penLogisticRegressionAuto(y, tX, K, lamb
     
     % We have now chosen the best lambda value, we can use all the provided
     % tX as train data to learn a model
-    beta = ridgeRegression(y, tX, lambdaStar);
+    beta = logisticRegression(y, tX, lambdaStar);
     
     % Plot evolution of train and test error with respect to lambda
     
